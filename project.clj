289bbb4695e0961388d0449d3f2f7810390d1841
Greg Haskins <greg@manetu.com>
2020-09-16 08:20:08 -0400

    Prepare for v1.5.8 development
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject protojure "1.5.7"
+(defproject protojure "1.5.8-SNAPSHOT"
   :description "Support library for protoc-gen-clojure, providing native Clojure support for Google Protocol Buffers and GRPC applications"
   :url "http://github.com/protojure/library"
   :license {:name "Apache License 2.0"
