756ca02d490daf0fc5470e8729ebbef1a380a7f5
Greg Haskins <greg@manetu.com>
2021-03-10 18:55:06 -0500

    Run lein-ancient
    We hold back protobuf due to an ABI breakage
    
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- project.clj
+++ project.clj
@@ -10,11 +10,11 @@
             [lein-kibit "0.1.8"]
             [lein-bikeshed "0.5.2"]
             [lein-cloverage "1.2.0"]]
-  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.10.3" :scope "provided"]
                  [org.clojure/core.async "1.3.610" :scope "provided"]
                  [com.google.protobuf/protobuf-java "3.13.0" :scope "provided"]
-                 [io.undertow/undertow-core "2.1.3.Final" :scope "provided"]
-                 [io.undertow/undertow-servlet "2.1.3.Final" :scope "provided"]
+                 [io.undertow/undertow-core "2.2.5.Final" :scope "provided"]
+                 [io.undertow/undertow-servlet "2.2.5.Final" :scope "provided"]
                  [org.eclipse.jetty.http2/http2-client "9.4.31.v20200723" :scope "provided"]
                  [org.eclipse.jetty/jetty-alpn-java-client "9.4.31.v20200723" :scope "provided"]
                  [io.pedestal/pedestal.log "0.5.8" :scope "provided"]
@@ -29,12 +29,11 @@
   :codox {:metadata {:doc/format :markdown}
           :namespaces [#"^(?!protojure.internal)"]}
   :profiles {:dev {:dependencies [[protojure/google.protobuf "0.9.1"]
-                                  [org.clojure/tools.namespace "1.0.0"]
-                                  [clj-http "3.10.2"]
+                                  [org.clojure/tools.namespace "1.1.0"]
+                                  [clj-http "3.12.1"]
                                   [com.taoensso/timbre "4.10.0"]
                                   [org.clojure/data.codec "0.1.1"]
                                   [org.clojure/data.generators "1.0.0"]
                                   [danlentz/clj-uuid "0.1.9"]
                                   [eftest "0.5.9"]]
                    :resource-paths ["test/resources"]}})
-
