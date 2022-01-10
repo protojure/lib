ef92e16dc5d077e5af0a612aabeecbccb3906975
Greg Haskins <greg@manetu.com>
2022-01-10 18:16:12 -0500

    Release v2.0.3
    Changes since v2.0.2
    --------------------
    9b2d069 Update deps
    
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- modules/grpc-server/project.clj
+++ modules/grpc-server/project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.protojure/grpc-server "2.0.3-SNAPSHOT"
+(defproject io.github.protojure/grpc-server "2.0.3"
   :description "GRPC server library for protoc-gen-clojure"
   :url "http://github.com/protojure/lib"
   :license {:name "Apache License 2.0"
--- modules/io/project.clj
+++ modules/io/project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.protojure/io "2.0.3-SNAPSHOT"
+(defproject io.github.protojure/io "2.0.3"
   :description "IO library to support io.github.protojure/core"
   :url "http://github.com/protojure/lib"
   :license {:name "Apache License 2.0"
