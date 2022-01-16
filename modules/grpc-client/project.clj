7b24a7b5c0fd7aa11661828bd198cd156a75f098
Greg Haskins <greg@manetu.com>
2022-01-16 13:35:33 -0500

    Release v2.0.5
    Changes since v2.0.4
    --------------------
    3758c8f Update deps
    
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- modules/grpc-client/project.clj
+++ modules/grpc-client/project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.protojure/grpc-client "2.0.5-SNAPSHOT"
+(defproject io.github.protojure/grpc-client "2.0.5"
   :description "GRPC client library for protoc-gen-clojure"
   :url "http://github.com/protojure/lib"
   :license {:name "Apache License 2.0"
