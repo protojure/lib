e821a9dd43a18edd79fbf7e3bee34e264c62c7b0
Greg Haskins <greg@manetu.com>
2022-03-02 12:54:14 -0500

    Release v2.0.9
    Mistagged release v2.0.8.  This release fixes the tag and bumps the version.
    
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- modules/core/project.clj
+++ modules/core/project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.protojure/core "v2.0.9-SNAPSHOT"
+(defproject io.github.protojure/core "2.0.9"
   :description "Core protobuf and GRPC utilities for protojure"
   :url "http://github.com/protojure/lib"
   :license {:name "Apache License 2.0"
