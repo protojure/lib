b6d5849f5c173c01bc2ccdd6ba24aab85858ea4a
Greg Haskins <greg@manetu.com>
2022-03-02 08:15:36 -0500

    Release v2.0.8
    Changes since v2.0.7
    ----------------------
    936bc4f [ADDED] Conform to pedestal json-body & body-params content-type expectations
    
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- modules/core/project.clj
+++ modules/core/project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.protojure/core "2.0.8-SNAPSHOT"
+(defproject io.github.protojure/core "v2.0.8"
   :description "Core protobuf and GRPC utilities for protojure"
   :url "http://github.com/protojure/lib"
   :license {:name "Apache License 2.0"
