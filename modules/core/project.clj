ae1c08513122c27628debeea5897de7ea7500784
Greg Haskins <greg@manetu.com>
2022-03-18 14:47:54 -0400

    Prepare for v2.1.1 development
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- modules/core/project.clj
+++ modules/core/project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.protojure/core "2.1.0"
+(defproject io.github.protojure/core "2.1.1-SNAPSHOT"
   :description "Core protobuf and GRPC utilities for protojure"
   :url "http://github.com/protojure/lib"
   :license {:name "Apache License 2.0"
