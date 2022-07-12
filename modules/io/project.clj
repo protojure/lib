498e446e6c0ab7fbe7bce15f61e1938452e8f863
Greg Haskins <greg@manetu.com>
2022-07-12 08:47:19 -0400

    Release v2.2.1
    Changes since v2.2.0
    -----------------------
    514bbd2 Update deps (#140)
    
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- modules/io/project.clj
+++ modules/io/project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.protojure/io "2.2.1-SNAPSHOT"
+(defproject io.github.protojure/io "2.2.1"
   :description "IO library to support io.github.protojure/core"
   :url "http://github.com/protojure/lib"
   :license {:name "Apache License 2.0"
