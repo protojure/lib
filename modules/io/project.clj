5678a8a6c8c1e9f784dd70473c95e234295607f4
Greg Haskins <greg@manetu.com>
2022-10-02 16:43:46 -0400

    Prepare for v2.3.3 development
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- modules/io/project.clj
+++ modules/io/project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.protojure/io "2.3.2"
+(defproject io.github.protojure/io "2.3.3-SNAPSHOT"
   :description "IO library to support io.github.protojure/core"
   :url "http://github.com/protojure/lib"
   :license {:name "Apache License 2.0"
