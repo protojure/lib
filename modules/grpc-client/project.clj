22753c97e5143d97dcf2cc5c29cb87e3d5bc3af2
Greg Haskins <greg@manetu.com>
2022-10-06 13:09:04 -0400

    Release v2.4.0
    Changes since v2.3.2
    --------------------
    b1d63c7 Add authz interceptor
    
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- modules/grpc-client/project.clj
+++ modules/grpc-client/project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.protojure/grpc-client "2.3.3-SNAPSHOT"
+(defproject io.github.protojure/grpc-client "2.4.0"
   :description "GRPC client library for protoc-gen-clojure"
   :url "http://github.com/protojure/lib"
   :license {:name "Apache License 2.0"
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(def protojure-version "2.3.3-SNAPSHOT")
+(def protojure-version "2.4.0")
 
 (defproject io.github.protojure/lib-suite "0.0.1"
   :description "Support libraries for protoc-gen-clojure, providing native Clojure support for Google Protocol Buffers and GRPC applications"
