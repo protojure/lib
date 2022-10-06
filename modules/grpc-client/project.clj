7f35a3520659dbe8e7ce0d659c9be1b34c1efada
Greg Haskins <greg@manetu.com>
2022-10-06 19:11:55 -0400

    Prepare for v2.4.2 development
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- modules/grpc-client/project.clj
+++ modules/grpc-client/project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.protojure/grpc-client "2.4.1"
+(defproject io.github.protojure/grpc-client "2.4.2-SNAPSHOT"
   :description "GRPC client library for protoc-gen-clojure"
   :url "http://github.com/protojure/lib"
   :license {:name "Apache License 2.0"
