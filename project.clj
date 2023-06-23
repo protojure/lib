31f826dabfaca7bf3adae996cb5d7a357d0f72e9
Greg Haskins <greg@manetu.com>
2023-06-23 09:56:54 -0400

    Prepare for v2.7.1 development
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(def protojure-version "2.7.0")
+(def protojure-version "2.7.1-SNAPSHOT")
 
 (defproject io.github.protojure/lib-suite "0.0.1"
   :description "Support libraries for protoc-gen-clojure, providing native Clojure support for Google Protocol Buffers and GRPC applications"
