689b65583f0533e8b527091e796c8eeb89cd2313
Greg Haskins <greg@manetu.com>
2023-09-05 14:15:44 -0400

    Release v2.7.1
    Changes since v2.7.0
    --------------------
    a42acdd [client] Add :insecure? flag
    61d8b4c Improve unit-tests for checking Empty support
    
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- modules/grpc-client/project.clj
+++ modules/grpc-client/project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.protojure/grpc-client "2.7.1-SNAPSHOT"
+(defproject io.github.protojure/grpc-client "2.7.1"
   :description "GRPC client library for protoc-gen-clojure"
   :url "http://github.com/protojure/lib"
   :license {:name "Apache License 2.0"
