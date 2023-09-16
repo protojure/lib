3ea8a7ab25f559fed8dc13b7c2c89257da455af2
Greg Haskins <greg@manetu.com>
2023-09-16 18:58:17 -0400

    Update dependencies
    We bump the minor version to 2.8.x to account for the major update in undertow to v2.3.x
    
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(def protojure-version "2.7.2-SNAPSHOT")
+(def protojure-version "2.8.0-SNAPSHOT")
 
 (defproject io.github.protojure/lib-suite "0.0.1"
   :description "Support libraries for protoc-gen-clojure, providing native Clojure support for Google Protocol Buffers and GRPC applications"
@@ -10,18 +10,19 @@
   :plugins [[lein-set-version "0.4.1"]
             [lein-sub "0.3.0"]]
   :managed-dependencies [[org.clojure/clojure "1.11.1"]
-                         [org.clojure/core.async "1.6.673"]
+                         [org.clojure/core.async "1.6.681"]
                          [org.clojure/tools.logging "1.2.4"]
-                         [com.google.protobuf/protobuf-java "3.22.2"]
-                         [org.apache.commons/commons-compress "1.23.0"]
+                         [com.google.protobuf/protobuf-java "3.24.3"]
+                         [org.apache.commons/commons-compress "1.24.0"]
                          [commons-io/commons-io "2.13.0"]
                          [funcool/promesa "9.2.542"]
-                         [io.undertow/undertow-core "2.2.25.Final"]
-                         [io.undertow/undertow-servlet "2.2.25.Final"]
+                         [javax.servlet/javax.servlet-api "4.0.1"]
+                         [io.undertow/undertow-core "2.3.8.Final"]
+                         [io.undertow/undertow-servlet "2.3.8.Final"]
                          [io.pedestal/pedestal.log "0.6.0"]
                          [io.pedestal/pedestal.service "0.6.0"]
-                         [org.eclipse.jetty.http2/http2-client "11.0.15"]
-                         [org.eclipse.jetty/jetty-alpn-java-client "11.0.15"]
+                         [org.eclipse.jetty.http2/http2-client "11.0.16"]
+                         [org.eclipse.jetty/jetty-alpn-java-client "12.0.1"]
                          [lambdaisland/uri "1.15.125"]
                          [io.github.protojure/io ~protojure-version]
                          [io.github.protojure/core ~protojure-version]
